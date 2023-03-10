258a0015bf5fb918bde1f025b6ef072275cd0fbe
bbgx <bernardo_bugmann@protonmail.com>
2023-03-09 23:01:47 -0300

    Add schedule lib to check the correct time to post

--- src/main.py
+++ src/main.py
@@ -2,18 +2,26 @@ import time
 from config import timezone, cache
 from utils import get_current_rate, compare_rates
 from datetime import datetime
+import schedule
 
-while True:
+
+def get_and_compare_rate():
     current_time = datetime.now(timezone)
-
-    if current_time.hour >= 10 and current_time.hour < 18:
+    print(current_time)
+    if current_time.hour >= 1 and current_time.hour < 23:
+        print('The bot is operating!')
         current_rate = get_current_rate()
 
         if current_time.hour == 10 and current_time.minute == 0:
+            print('It is 10am!')
             previous_rate = cache.get('previous_rate')
         else:
+            print('Posting the exchange rate!')
             previous_rate = compare_rates(cache.get('previous_rate'), current_rate)
             cache['previous_rate'] = previous_rate
 
+schedule.every().hour.at(":00").do(get_and_compare_rate)
 
-    time.sleep(3600)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ def get_current_rate():
     try:
         response = requests.get(BASE_URL)
         data = response.json()
+        print('Get the current rate!')
         return float(data["USDBRL"]["bid"])
     except:
         return None
@@ -13,20 +14,21 @@ def get_current_rate():
 def compare_rates(previous_rate, current_rate):
     if previous_rate is None:
         previous_rate = current_rate
+        print('Just saved the current rate to compare latter!')
     else:
         if round(current_rate, 2) > round(previous_rate, 2):
             event = Event(public_key=private_key.public_key.hex(), content=f"O dólar subiu! O dólar está R$ {current_rate:.2f}.")
             private_key.sign_event(event)
             relay_manager.publish_event(event)
-            print('Posted')
+            print('Just posted a new nostr post!')
         elif round(current_rate, 2) < round(previous_rate, 2):
             event = Event(public_key=private_key.public_key.hex(), content=f"O dólar caiu! O dólar está R$ {current_rate:.2f}.")
             private_key.sign_event(event)
             relay_manager.publish_event(event)
-            print('Posted')
+            print('Just posted a new nostr post!')
         elif round(current_rate, 2) == round(previous_rate, 2):
             event = Event(public_key=private_key.public_key.hex(), content=f"O dólar continua R$ {current_rate:.2f}. Não acho que quem ganhar ou quem perder, nem quem ganhar nem perder, vai ganhar ou perder. Vai todo mundo perder..")
             private_key.sign_event(event)
             relay_manager.publish_event(event)
-            print('Posted')
+            print('Just posted a new nostr post!')
     return current_rate
